cc00e2889be626045394eda2b28c1f803be56208
Shashi Kiran <shashi@intugine.com>
2021-02-05 22:55:37 +0530

    Initial commit

---
+++ application/main/routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, request
+from flask.templating import render_template
+from application.models import Post
+
+
+main = Blueprint('main', __name__)
+
+@main.route('/', methods=['GET'])
+@main.route('/homepage', methods=['GET'])
+def home():
+    page = request.args.get('page', type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('home.html', posts=posts)
+
