1fb5f38501725e8d2447b82a579a26f8edbdf24f
Shashi Kiran <shashikiranr78@gmail.com>
2021-02-23 22:23:44 +0530

    initial commit

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,12 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from application.config import Config
 from flask_admin import Admin
+import boto3
+from flask_s3 import FlaskS3
 
 
 db = SQLAlchemy()
@@ -12,16 +15,20 @@ login_manager = LoginManager()
 admin = Admin()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-
+s3 = FlaskS3()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
+    app.config['FLASKS3_BUCKET_NAME'] = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+    app.config['AWS_ACCESS_KEY_ID'] = os.environ.get('AWS_ACCESS_KEY_ID')
+    app.config['AWS_SECRET_ACCESS_KEY'] = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     admin.init_app(app)
+    s3.init_app(app)
     
     from application.models import User, Post, Transaction, MyModelView
     admin.add_view(MyModelView(User, db.session))
