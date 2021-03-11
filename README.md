88bf92878006fc47d721475e2edc612906dc7f08
Shashi Kiran <shashikiranr78@gmail.com>
2021-03-11 20:31:31 +0530

    Added Readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # farmer_website
-A website for farmers
+Raithu Nestham is for the new age farmer. It is aimed at providing the necessary tools that help farmer in saving valuable manpower and bring automation into their lives.
+
+Built using flask
+
+To download required packages
+pip install -r requirements.txt
+
+To run
+python run.py
+
+Can also be viewed at URL
+https://raithunestham.herokuapp.com/
--- application/users/forms.py
+++ application/users/forms.py
@@ -33,7 +33,6 @@ class LoginForm(FlaskForm):
 class UpdateAccountForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators = [DataRequired(), Email()])
-    picture = FileField('Update Profile Picture', validators = [FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
 
     def validate_username(self, username):
